c8c26757684c149d99ad9784e45b69572b0904e7
Nikitooos123 <120833063+Nikitooos123@users.noreply.github.com>
2024-01-16 23:30:59 +0300

    убран лишний урл адрес

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('post_complete/', views.post_complete, name='post_complete'),
     path('user=<int:id>/', views.profiles, name='profiles'),
     path('post:<str:id>/', views.post_detail, name='post_detail'),
     path('like/', views.like, name='likes'),
